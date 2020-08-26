df825f6154da293a1ebd4aa99b8ffb1504e804b1
ZirariHamza <zirari.hamza@gmail.com>
2020-08-26 18:49:11 +0100

    WS commit

---
+++ demo/src/main/java/com/java/soap/demo/SpringBootSoapWebApplication.java
@@ -0,0 +1,13 @@
+package com.java.soap.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSoapWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSoapWebApplication.class, args);
+	}
+
+}
